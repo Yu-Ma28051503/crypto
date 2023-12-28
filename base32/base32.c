2d57a29fce1f306c6842215f3149b6bb86c7f604
yuma <matsuni@v6.netsci.info.hiroshima-cu.ac.jp>
2023-12-28 15:42:03 +0900

    base32の追加

---
+++ base32/base32.c
@@ -0,0 +1,26 @@
+/*
+ * base32を実装したプログラム
+ *
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 100
+
+int main(void)
+{
+    char str[MAX];
+    char *p;
+
+    printf("Input string: ");
+    fgets(str, MAX, stdin);
+
+    p = str;
+
+    
+
+    return 0;
+}
